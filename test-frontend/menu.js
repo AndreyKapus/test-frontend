f93fbdde067dc62eb71ca16d4365d1552632ae2c
AndreyKapus <kapustnikov@ukr.net>
2023-08-27 16:22:40 +0300

    animation

--- test-frontend/menu.js
+++ test-frontend/menu.js
@@ -1,14 +1,30 @@
 (() => {
     const refs = {
+      headerToMove: document.querySelector('.header'),
       showBackdrop: document.querySelector(".burger-menu"),
       closeDropBtn: document.querySelector(".close-drop-btn"),
       navMenu: document.querySelector(".nav-wrapper"),
+      footerMove: document.querySelector('.c-footer'),
     };
   
-    refs.showBackdrop.addEventListener("click", toggleModal);
-    refs.closeDropBtn.addEventListener("click", toggleModal);
+    refs.showBackdrop.addEventListener("click", toggleMenu);
+    refs.closeDropBtn.addEventListener("click", toggleMenu);
+
+    const addHeaderMove = () => {
+      refs.headerToMove.classList.add('header-move');
+      refs.footerMove.classList.add('footer-move');
+    };
+
+    const removeChangePosition = () => {
+      refs.headerToMove.classList.remove('header-move')
+      refs.footerMove.classList.remove('footer-move');
+    }
   
-    function toggleModal() {
+    function toggleMenu() {
       refs.navMenu.classList.toggle("is-hidden");
+      if(!refs.navMenu.classList.contains('is-hidden')) {
+       return addHeaderMove()
+      }
+     return removeChangePosition()
     }
   })();
